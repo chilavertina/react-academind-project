710a0d8e8de833e122c29d421442e33208f407cd
chilavertina <urosmartic93@gmail.com>
2023-03-18 13:08:36 +0100

    komunikacija komponenti od child ka parent

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,16 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Expenses.css";
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
 
 function Expenses(props) {
+  const [filteredYear, setFilteredYear] = useState("");
+
+  const pickedYearDataHandler = (enteredPickedYearData) => {
+    setFilteredYear(enteredPickedYearData);
+  };
+
   return (
-    <Card className="expenses">
-      <ExpenseItem title={props.troskovi[0].title} amount={props.troskovi[0].amount} date={props.troskovi[0].date} />
-      <ExpenseItem title={props.troskovi[1].title} amount={props.troskovi[1].amount} date={props.troskovi[1].date} />
-      <ExpenseItem title={props.troskovi[2].title} amount={props.troskovi[2].amount} date={props.troskovi[2].date} />
-      <ExpenseItem title={props.troskovi[3].title} amount={props.troskovi[3].amount} date={props.troskovi[3].date} />
-    </Card>
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter selected={filteredYear} onSavePickedYearData={pickedYearDataHandler} />
+        <ExpenseItem title={props.troskovi[0].title} amount={props.troskovi[0].amount} date={props.troskovi[0].date} />
+        <ExpenseItem title={props.troskovi[1].title} amount={props.troskovi[1].amount} date={props.troskovi[1].date} />
+        <ExpenseItem title={props.troskovi[2].title} amount={props.troskovi[2].amount} date={props.troskovi[2].date} />
+        <ExpenseItem title={props.troskovi[3].title} amount={props.troskovi[3].amount} date={props.troskovi[3].date} />
+      </Card>
+    </div>
   );
 }
 
