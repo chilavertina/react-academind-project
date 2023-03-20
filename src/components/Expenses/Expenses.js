5d9514b037e65edfa8619ec4518826d7066a8fb4
chilavertina <urosmartic93@gmail.com>
2023-03-20 20:46:09 +0100

    filtriranje podataka uz pomoc godine

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -11,12 +11,16 @@ function Expenses(props) {
     setFilteredYear(enteredPickedYearData);
   };
 
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
   return (
     <div>
       <Card className="expenses">
         <ExpensesFilter selected={filteredYear} onSavePickedYearData={pickedYearDataHandler} />
-        {props.items.map((expense) => (
-          <ExpenseItem title={expense.title} amount={expense.amount} date={expense.date} />
+        {filteredExpenses.map((expense) => (
+          <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date} />
         ))}
       </Card>
     </div>
