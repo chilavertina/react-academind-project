2f9b10b9d3d46041bed03bec41b0276569a87a9b
chilavertina <urosmartic93@gmail.com>
2023-03-20 19:27:40 +0100

    ubacivanje novih proizvoljnih podataka u listu

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -15,7 +15,7 @@ function Expenses(props) {
     <div>
       <Card className="expenses">
         <ExpensesFilter selected={filteredYear} onSavePickedYearData={pickedYearDataHandler} />
-        {props.troskovi.map((expense) => (
+        {props.items.map((expense) => (
           <ExpenseItem title={expense.title} amount={expense.amount} date={expense.date} />
         ))}
       </Card>
