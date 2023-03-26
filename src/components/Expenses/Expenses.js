3e3778ab32b842da5bb313d2290040eb889d4362
chilavertina <urosmartic93@gmail.com>
2023-03-26 12:41:43 +0200

    dodavanje chart bar-a

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -3,6 +3,7 @@ import "./Expenses.css";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 
 function Expenses(props) {
   const [filteredYear, setFilteredYear] = useState("");
@@ -19,6 +20,7 @@ function Expenses(props) {
     <li>
       <Card className="expenses">
         <ExpensesFilter selected={filteredYear} onSavePickedYearData={pickedYearDataHandler} />
+        <ExpensesChart expenses={filteredExpenses} />
         <ExpensesList items={filteredExpenses} />
       </Card>
     </li>
